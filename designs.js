767d27c880fe6d6fc5f79eed2b5604d88bce3133
Tutu <fxiaofan@gmail.com>
2017-11-17 23:27:24 -0500

    Finished the basic function
    Need to fix:
    1. whether we need id for each cell?
    2. whether to check the input value?

--- designs.js
+++ designs.js
@@ -3,8 +3,30 @@
 
 // When size is submitted by the user, call makeGrid()
 
+
+$("input:submit").on("click",
+    function(evt){
+        evt.preventDefault();
+        makeGrid();
+    });
+
+
 function makeGrid() {
-
-// Your code goes here!
-
-}
+    //console.log(jQuery.type($("#input_height").val()));
+    const rowSize = parseInt($("#input_height").val());
+    const columnSize = parseInt($("#input_width").val())
+    $("#pixel_canvas").empty();
+    let row, column;
+    for (row = 0; row < rowSize; row++){
+        $("#pixel_canvas").append("<tr id=row"+row+"></tr>");
+        for (column = 0; column< columnSize; column++){
+            var cellNum = row*columnSize + column;
+            $("#row"+row).append("<td id= cell"+ cellNum+"></td>");
+        }
+    }
+    console.log($("#colorPicker").val());
+    $("td").click(function (evt){
+        console.log(evt.target.id);
+        $(evt.target).css("background-color", $("#colorPicker").val());
+    })
+};
